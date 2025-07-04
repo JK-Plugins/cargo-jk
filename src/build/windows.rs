77df84a211b4b8b3a013d9d2e2f94d560018cabb
naari3 <naari.named@gmail.com>
2025-07-04 22:44:32 +0900

    Windows版で正しくJkPluginMetadataを使うように修正

--- src/build/windows.rs
+++ src/build/windows.rs
@@ -1,17 +1,17 @@
 use std::path::{Path, PathBuf};
 
-use crate::command::Build;
+use crate::{command::Build, JkPluginMetadata};
 
 pub fn post_build_process<P: AsRef<Path>>(
     _build: &Build,
     filename: P,
     _package_name: &str,
-    plugin_name: &str,
+    jk_plugin_metadata: &JkPluginMetadata,
 ) -> PathBuf {
     let dllfilepath = filename.as_ref().to_path_buf();
     let dllfiledir = dllfilepath.parent().unwrap();
     // rename the DLL file to the plugin name
-    let new_dll_path = dllfiledir.join(&plugin_name).with_extension("aex");
+    let new_dll_path: PathBuf = dllfiledir.join(&jk_plugin_metadata.plugin_name).with_extension("aex");
     std::fs::rename(dllfilepath, &new_dll_path).expect("Failed to rename DLL file");
     eprintln!("Renamed DLL to: {}", new_dll_path.display());
 
